026d6ee04a2800d090a037350501ebca9587816f
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-06-21 10:55:00 +0200

    Add the correction

--- src/Service/Container.php
+++ src/Service/Container.php
@@ -6,6 +6,20 @@ class Container
 {
     public function inbox(int $cupcakes): array
     {
-        return [];
+        $largeBox = 0;
+
+        while($cupcakes > 8) {
+            $largeBox++;
+            $cupcakes -= 8;
+        }
+        
+        if($cupcakes === 1) return [$largeBox + 0, 0, 1];
+        if($cupcakes === 2) return [$largeBox + 0, 0, 1];
+        if($cupcakes === 3) return [$largeBox + 0, 1, 0];
+        if($cupcakes === 4) return [$largeBox + 0, 1, 0];
+        if($cupcakes === 5) return [$largeBox + 0, 1, 0];
+        if($cupcakes === 6) return [$largeBox + 1, 0, 0];
+        if($cupcakes === 7) return [$largeBox + 1, 0, 0];
+        if($cupcakes === 8) return [$largeBox + 1, 0, 0];
     }
 }
